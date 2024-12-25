d0e2780ab8fb78698e03279d92f2b2578cbdfc90
jyoti1911sngwn <145776387+jyoti1911sngwn@users.noreply.github.com>
2024-12-25 07:01:04 -0800

    Update db.js

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const mongoose= require('mongoose');
 // const mongoURI ="mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass%directConnection=true&ssl=false"
 
 const connectToMongo = () =>{
-    mongoose.connect("mongodb+srv://sangwanjyoti717:v9IsWxIFN8koFpkX@cluster0.i1dux.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", { useNewUrlParser: true,
+    mongoose.connect("mongodb+srv://userauthenticate:6E9wKGx-r3Qz7Z6@cluster0.i1dux.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", { useNewUrlParser: true,
         useUnifiedTopology: true,
         serverSelectionTimeoutMS: 10000, }).then(() => {
   console.log("database cnnected");}).catch((err) => {
